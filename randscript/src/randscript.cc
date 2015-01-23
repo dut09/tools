451b0aacb6af4cb97ad4dc9cfd0c2f4efde3bbdb
dut09 <dutao09@gmail.com>
2015-01-22 22:34:50 -0800

    Finish geometry. Read vertices and faces from the obj file. Tested with example/cube.obj.

--- randscript/src/randscript.cc
+++ randscript/src/randscript.cc
@@ -6,109 +6,7 @@
 using namespace std;
 
 // 100 BRDF names.
-#define MATERIAL_NUM 100
-const string materials[MATERIAL_NUM] = {
-  "alum-bronze",
-  "alumina-oxide",
-  "aluminium",
-  "aventurnine",
-  "beige-fabric",
-  "black-fabric",
-  "black-obsidian",
-  "black-oxidized-steel",
-  "black-phenolic",
-  "black-soft-plastic",
-  "blue-acrylic",
-  "blue-fabric",
-  "blue-metallic-paint",
-  "blue-metallic-paint2",
-  "blue-rubber",
-  "brass",
-  "cherry-235",
-  "chrome-steel",
-  "chrome",
-  "colonial-maple-223",
-  "color-changing-paint1",
-  "color-changing-paint2",
-  "color-changing-paint3",
-  "dark-blue-paint",
-  "dark-red-paint",
-  "dark-specular-fabric",
-  "delrin",
-  "fruitwood-241",
-  "gold-metallic-paint",
-  "gold-metallic-paint2",
-  "gold-metallic-paint3",
-  "gold-paint",
-  "gray-plastic",
-  "grease-covered-steel",
-  "green-acrylic",
-  "green-fabric",
-  "green-latex",
-  "green-metallic-paint",
-  "green-metallic-paint2",
-  "green-plastic",
-  "hematite",
-  "ipswich-pine-221",
-  "light-brown-fabric",
-  "light-red-paint",
-  "maroon-plastic",
-  "natural-209",
-  "neoprene-rubber",
-  "nickel",
-  "nylon",
-  "orange-paint",
-  "pearl-paint",
-  "pickled-oak-260",
-  "pink-fabric",
-  "pink-fabric2",
-  "pink-felt",
-  "pink-jasper",
-  "pink-plastic",
-  "polyethylene",
-  "polyurethane-foam",
-  "pure-rubber",
-  "purple-paint",
-  "pvc",
-  "red-fabric",
-  "red-fabric2",
-  "red-metallic-paint",
-  "red-phenolic",
-  "red-plastic",
-  "red-specular-plastic",
-  "silicon-nitrade",
-  "silver-metallic-paint",
-  "silver-metallic-paint2",
-  "silver-paint",
-  "special-walnut-224",
-  "specular-black-phenolic",
-  "specular-blue-phenolic",
-  "specular-green-phenolic",
-  "specular-maroon-phenolic",
-  "specular-orange-phenolic",
-  "specular-red-phenolic",
-  "specular-violet-phenolic",
-  "specular-white-phenolic",
-  "specular-yellow-phenolic",
-  "ss440",
-  "steel",
-  "teflon",
-  "tungsten-carbide",
-  "two-layer-gold",
-  "two-layer-silver",
-  "violet-acrylic",
-  "violet-rubber",
-  "white-acrylic",
-  "white-diffuse-bball",
-  "white-fabric",
-  "white-fabric2",
-  "white-marble",
-  "white-paint",
-  "yellow-matte-plastic",
-  "yellow-paint",
-  "yellow-phenolic",
-  "yellow-plastic",
-};
+#include "materials.h"
 
 struct Vector3D {
   Vector3D() : x(0.0), y(0.0), z(0.0) {}
@@ -189,18 +87,58 @@ int main(int argc, char* argv[]) {
   pbrtScriptFile << endl;
   pbrtScriptFile << "WorldBegin" << endl << endl;
 
-  // Lighting.
+  // Initialize random seed.
+  srand(time(NULL));
 
-  // Material.
+  // Lighting section.
+
+  // Material section.
   pbrtScriptFile << endl;
   pbrtScriptFile << "AttributeBegin" << endl;
+
   // Randomly pick a material.
-  srand(time(NULL));
   string materialName = materials[rand() % MATERIAL_NUM];
   pbrtScriptFile << "Material \"measured\" \"string filename\" \""
                  << merlFolder << "/" << materialName << ".binary\"" << endl;
-  // Geometry.
 
+  // Geometry section.
+  pbrtScriptFile << endl;
+
+  // Open up obj file.
+  // Assume the obj file format is:
+  // v v1x v1y v1z
+  // v v2x v2y v2z
+  // ...
+  // f f1i f1j f1k
+  // f f2i f2j f2k
+  // ...
+  ifstream objFile;
+  objFile.open(objFileName.c_str());
+  pbrtScriptFile << "Shape \"trianglemesh\"" << endl;
+  pbrtScriptFile << "\"point P\" [" << endl;
+
+  // Read points from obj file.
+  char ch;
+  Vector3D point;
+  objFile >> ch;
+  while (ch == 'v') {
+    objFile >> point.x >> point.y >> point.z;
+    pbrtScriptFile << "  " << point.x << " " << point.y << " "
+                   << point.z << endl;
+    objFile >> ch;
+  }
+  pbrtScriptFile << "]" << endl;
+
+  // Read faces.
+  pbrtScriptFile << "\"integer indices\" [" << endl;
+  while (!objFile.eof() && ch == 'f') {
+    objFile >> point.x >> point.y >> point.z;
+    pbrtScriptFile << "  " << (int)(point.x - 1) << " " << (int)(point.y - 1)
+                   << " " << (int)(point.z - 1) << endl;
+    objFile >> ch;
+  }
+  pbrtScriptFile << "]" << endl;
+  objFile.close();
   pbrtScriptFile << "AttributeEnd" << endl;
 
   /////////////////////////////////////////////////////////////////////////////
